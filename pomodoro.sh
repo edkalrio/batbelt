35415a38e93758e5ca1ee4a9d947a564498776ec
Eduardo Calvo <edkalrio@lunaticgeek.com>
2018-09-12 21:29:50 +0200

    it's the final countdown

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,14 +1,17 @@
-/bin/env bash
+#!/bin/env bash
 t() {
-	sleep $(($1*60))
-	echo -e "\a"
+	for ((i=$1*1;i>=0;i--)); do 
+		printf '\r%02d:%02d' $(($i%3600/60)) $(($i%60))
+		sleep 1
+	done
+	echo -ne "\r\a"
 }
 
 c=0
 while true; do
 	t 25
 	((c++))
-	if [ $(( $c % 4 )) -eq 0 ]; then
+	if [ $((c%4)) -eq 0 ]; then
 		t 30
 	else
 		t 5
