6932791eeb11c59b4f52ddf0ff97c00e2d260527
Eduardo Calvo <edkalrio@lunaticgeek.com>
2019-02-15 13:15:30 +0100

    now with extra bashisms

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,17 +1,17 @@
 #!/bin/env bash
+
 t() {
-	for ((i=$1*1;i>=0;i--)); do 
-		printf '\r%02d:%02d' $(($i%3600/60)) $(($i%60))
+	for ((i=$1*60;i>=0;i--)); do 
+		printf '\r%02d:%02d' $((i%3600/60)) $((i%60))
 		sleep 1
 	done
 	echo -ne "\r\a"
 }
 
 c=0
-while true; do
+for ((;;c++)); do
 	t 25
-	((c++))
-	if [ $((c%4)) -eq 0 ]; then
+	if ((c%4==0)); then
 		t 30
 	else
 		t 5
